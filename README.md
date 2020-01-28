476ffcc2b56637162d9f108a57c4f7bb060f52aa
Antoine GANNAT <antoine.gannat@genesys.com>
2020-01-28 11:41:34 +0100

    improve code + readme

--- README.md
+++ README.md
@@ -1,16 +1,55 @@
-
-
-
 # auto-filling-by-the-code
-The purpose of this project consists in filling a table in the Readme.md by a Github action based on a JSON file updated.
+The purpose of this project is to fill a compatibility table showing the compatibility between a project and its dependencies.
 
+The versions are loaded from a JSON file : ``compatibility-versions.json``
 
-# This is the table updated by the script
+# Compatibility table
 
-Project version|Component A version|Component B version|Component C version|
-|---|---|---|---|
-|1.0|2.8|3.4|5.8|
-|1.1|2.9|3.7|5.8|
-|2.0|2.9|4.4|6.5|
-|2.1|3.9|4.5|6.8|
-|2.5|5.9|5.5|7.8|
+|Project version|ComponentA version|ComponentB version|ComponentC version|ComponentE version|ComponentD version|
+|---|---|---|---|---|---|
+|1.0|2.7|3.4|5.8|-|-|
+|1.1|2.9|3.7|5.8|0.1|-|
+|2.0|2.9|4.4|6.5|-|8.1|
+|2.1|3.9|4.5|6.8|-|-|
+|2.5|5.9|5.5|7.8|-|-|
+
+The table above will be updated automaticly when ``compatibility-versions.json`` is modified.
+
+# JSON file
+
+Here is an example of ``compatibility-versions.json``
+
+    [
+        {
+            "Project": "1.0",
+            "ComponentA": "2.7",
+            "ComponentB": "3.4",
+            "ComponentC": "5.8"  
+        },
+        {
+            "Project": "1.1",
+            "ComponentA": "2.9",
+            "ComponentB": "3.7",
+            "ComponentC": "5.8",
+            "ComponentE": "0.1" 
+        },
+        {
+            "Project": "2.0",
+            "ComponentA": "2.9",
+            "ComponentB": "4.4",
+            "ComponentC": "6.5",
+            "ComponentD": "8.1"
+        },
+        {
+            "Project": "2.1",
+            "ComponentA": "3.9",
+            "ComponentB": "4.5",
+            "ComponentC": "6.8"  
+        },
+        {
+            "Project": "2.5",
+            "ComponentA": "5.9",
+            "ComponentB": "5.5",
+            "ComponentC": "7.8"  
+        }
+    ]
--- ci/update-versions-in-README-md.js
+++ ci/update-versions-in-README-md.js
@@ -2,40 +2,117 @@
 
 const fs = require('fs');
 
-let rawDataVersion = fs.readFileSync(__dirname + '/../compatibility-versions.json');
-let versions = JSON.parse(rawDataVersion);
+// get the versions file and parse it to JSON
+let versions = JSON.parse(fs.readFileSync(__dirname + '/../compatibility-versions.json'));
+// get the readme file
 let readme = fs.readFileSync(__dirname + '/../README.md').toString('utf8');
-console.log(versions);
+// parse the file to lines
 let lines = readme.split(/\r\n|\n|\r/);
-let blockToReplace = false;
-let newReadme = '';
 
+// generate a markdown table from the versions file
 function generateTheTable() {
-    var result = `
-Project version|Component A version|Component B version|Component C version|
-|---|---|---|---|`
-    for(let i = 0;i < versions.length;i++) {
-        result += '\n|' + versions[i].projectVersion + '|' + versions[i].componentAVersion + '|' + versions[i].componentBVersion + '|' + versions[i].componentCVersion + '|';
+    // return an array containing the name of every component
+    function getEveryComponent(){
+        let components = []
+        versions.forEach((version) => {
+            // for each key in this version
+            for (let key in version){
+                // if this key is not in the components array, add it 
+                if (!components.includes(key)){
+                    components.push(key)
+                }
+            }
+        })
+        return (components);
     }
+    // add the table header and a separator to 'result'
+    function setHeader(components, dest){
+        // init the header
+        let header = '|';
+        // init the separator
+        let separator = '|';
+        // for each component
+        components.forEach((component) => {
+            // add the component name to the header
+            header += component + ' version|';
+            // add a separator
+            separator += '---|';
+        })
+        // push the header
+        dest.push(header);
+        // push the separator
+        dest.push(separator);
+    }
+    // create a result array
+    let result = [];
+    // find every component
+    const components = getEveryComponent();
+    // add an empty line first
+    result.push('');
+    // add the header and the separation between the header and the data
+    setHeader(components, result);
+    // for each version, add the compatibility between each component
+    versions.forEach((version) => {
+        // init the line
+        let line = '|';
+        // for each known component
+        components.forEach((component) => {
+            // if the component is not present for this version
+            if (!version[component]){
+                // add a dash
+                line += '-|';
+            }
+            else{
+                // otherwise, add the component version
+                line += version[component] + '|';
+            }
+        })
+        // push the line to the result array
+        result.push(line);
+    })
+    // return the array
     return result;
 }
 
-for (let i=0;i < lines.length;i++) {
-    if (blockToReplace) {
-        if (lines[i].indexOf('#') === 0 ) {
-            blockToReplace = false;
+// find the current version table in the readme file, and replace it by the new one
+function replaceCurrentTable(){
+    // line used to identify the position of the table
+    const tableTitle = '# Compatibility table';
+    // search the line where the compatibility table title is present
+    const tableTitleIndex = lines.findIndex((l) => l.indexOf(tableTitle) >= 0);
 
-            newReadme+= '\n' + generateTheTable() + '\n' + lines[i]
-        }
-    } else {
-        if (lines[i].indexOf('# This is the table updated by the script') === 0) {
-            blockToReplace = true;
-        }
-        newReadme+= '\n' + lines[i]
-
+    // if the line is not found, add at the bottom of the file
+    if (tableTitleIndex < 0){
+        // add the title
+        readme += '\n# Compatibility table\n';
+        // add the table (joined by line breaks)
+        readme += generateTheTable().join('\n');
+        return;
     }
+
+    // variable checking if a non-empty line has already been found
+    let nonEmptyLineFound = false;
+    // init the line search at the line after the title
+    let i = tableTitleIndex + 1;
+    // find the lines on which the table is located
+    // this will search until an empty line is found (a line with content must be found first)
+    for (; i < lines.length && (!nonEmptyLineFound || lines[i].length > 0); i++){
+        // if a line with content if found
+        if (!nonEmptyLineFound && lines[i].length > 0){
+            nonEmptyLineFound = true
+        }
+    }
+    // remove the old table
+    lines.splice(tableTitleIndex + 1, i - tableTitleIndex - 1);
+
+    // insert the new one
+    lines.splice(tableTitleIndex + 1, 0, generateTheTable().join('\n'));
+
+    // convert the array back to a string
+    readme = lines.join('\n');
 }
-if (blockToReplace) {
-    newReadme += '\n' + generateTheTable();
-}
-fs.writeFileSync(__dirname + '/../README.md', newReadme)
+
+// replace the current table by a new one
+replaceCurrentTable();
+// Write over to the readme file
+fs.writeFileSync(__dirname + '/../README.md', readme)
